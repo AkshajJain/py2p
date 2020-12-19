8272b03b76cb72d2a4a34adf3c684458bf10ba74
AkshajJain <akshajj2004@gmail.com>
2020-12-19 10:56:08 +0530

    Akshaj: testing

--- p2p.py
+++ p2p.py
@@ -47,8 +47,10 @@ ip_table = { }
 req_table = { }
 
 #socketManager = selectors.DefaultSelector(  )
-listener = socket.socket( family = socket.AF_INET, type = socket.SOCK_DGRAM )
+listener = socket.socket( family = socket.AF_INET, type = socket.SOCK_STREAM )
 listener.bind(("", selfPort))
+listener.settimeout(0.1)
+listener.listen()
 
 whitelisted_ips = ["106.201.123.139", "106.200.238.248", "49.207.201.183", "49.37.170.237", "49.207.201.250", "171.61.90.0", "49.207.223.177"]
 
@@ -56,42 +58,25 @@ whitelisted_ips = ["106.201.123.139", "106.200.238.248", "49.207.201.183", "49.3
 listener.setblocking(False)
 
 socketManager = selectors.DefaultSelector()
-socketManager.register(listener, selectors.EVENT_READ, True)
+socketManager.register(listener, selectors.EVENT_READ, acc)
 
 print('I am', (selfIp, selfPort))
 
-def networking( ):
-    for ip in whitelisted_ips:
-        listener.sendto(b'', (ip, selfPort))
-    for key in req_table:
-        if(not (ip_table.get(key, None) is None)):
-            for query in req_table[key]:
-                print(query)
-                msg = query[0] + ';' + query[1] + ';'
-                if(query[0] == 'create'):
-                    for UID in query[2]:
-                        msg += UID + ';'
-                    msg = msg[:-1:]
 
-                elif(query[0] == 'remove'):
-                    msg += query[2]
+def acc(sock):
+    global ip_table, req_table
+    conn, addr = sock.accept()
+    print("conn:", conn)
+    print("addr:", addr)
+    conn.setblocking(False)
+    socketManager.register(conn, selectors.EVENT_READ, read)
 
-                elif(query[0] == 'recv'):
-                    msg += query[2] + ';' + query[3]
-                  
-                elif(query[0] == 'addper'):
-                    msg += query[2]
-
-                listener.sendto(msg.encode('utf8'), ip_table[key])
-
-            req_table[key] = []
-
-    events = socketManager.select(timeout = 0.01)
-    for(key, mask) in events:
-        msg, addr = listener.recvfrom(BUFSIZ)
-        print('msg:', msg)
-        print('addr:', addr)
-        if not msg : continue
+def read(sock):
+    global ip_table, req_table
+    msg, addr = sock.recvfrom(BUFSIZ)
+    print('msg:', msg)
+    print('addr:', addr)
+    if msg:
         msg = (msg.decode()).split(';')
 
         sender = msg[0] # This is the UID of the sender
@@ -99,13 +84,14 @@ def networking( ):
 
         # Add phone number to table
         if(query == 'online'):
-            ip_table.setdefault(sender, addr)
+            ip_table.setdefault(sender, sock)
             print(ip_table)
 
         # Remove phone number to table
         elif(query == 'ofline'):
             try:
                 ip_table.pop(sender)
+                sock.close()
                 print(ip_table)
             except Exception as e:
                 print(ip_table)
@@ -149,7 +135,7 @@ def networking( ):
             if(req_table.get(target, None) is None):
                 req_table[target] = []
             req_table[target].append(['recv', name, sender, data])
-        
+
         elif(query == 'addper'):
 
             target = msg[2]
@@ -159,5 +145,41 @@ def networking( ):
             if(req_table.get(target, None) is None):
                 req_table[target] = []
             req_table[target].append(['addper', name, toAdd])
+
+
+def networking( ):
+    global whitelisted_ips
+    for ip in whitelisted_ips:
+        try:
+            listener.connect((ip, selfPort))
+        except Exception as e:
+            pass
+    for key in req_table:
+        if(not (ip_table.get(key, None) is None)):
+            for query in req_table[key]:
+                print(query)
+                msg = query[0] + ';' + query[1] + ';'
+                if(query[0] == 'create'):
+                    for UID in query[2]:
+                        msg += UID + ';'
+                    msg = msg[:-1:]
+
+                elif(query[0] == 'remove'):
+                    msg += query[2]
+
+                elif(query[0] == 'recv'):
+                    msg += query[2] + ';' + query[3]
+
+                elif(query[0] == 'addper'):
+                    msg += query[2]
+
+                ip_table[key].sendall(msg.encode('utf8'))
+
+            req_table[key] = []
+
+    events = socketManager.select(timeout = 0.01)
+    for(key, mask) in events:
+        key.data(key.fileobj)
+
 
 while True: networking( )
